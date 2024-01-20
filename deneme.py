28997bf06558bf7f2ea723af27490f33e9d48b2a
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2024-01-21 01:00:39 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,7 +1,36 @@
+import os
 import pandas as pd
+from datetime import datetime
 
-df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
-                   'mask': ['red', 'purple'],
-                   'weapon': ['sai', 'bo staff']})
-  
-df.to_csv('https://github.com/kovacs5/fotmob_csv/blob/9300862180c56edb00c7b88d0998fc129ba3911f/out.csv')
+def write_attendance():
+  user = input()
+  path = "./generated_files/" + user + '/'
+
+  if not os.path.exists(path):
+    os.makedirs(path)
+
+  now = datetime.now()
+  date = now.strftime("%Y-%m-%d")
+  time = now.strftime("%H:%M:%S")
+
+  ym = now.strftime("%Y-%m")
+  fn = path + user + '-' + ym + '.csv'
+
+  if not os.path.exists(fn):
+    df = pd.DataFrame(columns=['Reg. No.', 'Date', 'Time'])
+    df.to_csv(fn, index=False)
+
+  df = pd.read_csv(fn)
+  lst = df['Date'].tolist()
+
+  if date in lst:
+    print('You have already gave your attendance.')
+  else:
+    df.loc[len(df)] = [user, date, time]
+    df.to_csv(fn, index=False)
+    print('Your attendance has been updated.')
+    print('{}'.format(df))
+
+
+if __name__ == '__main__':
+  write_attendance()
