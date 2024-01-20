c47127ab3b1e4063233a09e5c83b9215dc27176f
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2024-01-21 00:52:13 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -4,4 +4,4 @@ df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
                    'mask': ['red', 'purple'],
                    'weapon': ['sai', 'bo staff']})
   
-df.to_csv('./out.csv')
+df.to_csv('https://github.com/kovacs5/fotmob_csv/blob/main/out.csv')
