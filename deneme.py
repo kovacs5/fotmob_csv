ca49b1b85790e45cc39c94264a9df524fe74be55
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2024-01-21 00:29:34 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import os
 
 df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
                    'mask': ['red', 'purple'],
                    'weapon': ['sai', 'bo staff']})
-
-os.makedirs('csv', exist_ok=True)  
-df.to_csv('csv/out.csv')
+  
+df.to_csv('fotmob_csv/out.csv')
