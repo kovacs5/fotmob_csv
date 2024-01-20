6367a9a5f735af0f673dab12e156ed3498e03b00
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2024-01-21 01:06:59 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
                    'mask': ['red', 'purple'],
-                   'weapon': ['sai', 'bo staff']})3
+                   'weapon': ['sai', 'bo staff']})
 
 df.to_csv('out.csv')
