b3ec93839541b9bd4973b7862752ff968ca50399
kovacs5 <126562222+kovacs5@users.noreply.github.com>
2024-02-11 01:15:11 +0300

    Update finalcsv_creator.py

--- finalcsv_creator.py
+++ finalcsv_creator.py
@@ -5,41 +5,34 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.fotmob.com/leagues/71/matches/super-lig'
+json_url = "https://www.fotmob.com/api/leagues?id=71&ccode3=TUR"
 
-r = requests.get(url)
+with urllib.request.urlopen(json_url) as url:
+    data = json.load(url)
 
-soup = BeautifulSoup(r.text, 'html.parser')
+table = data['matches']['allMatches']
+df = pd.json_normalize(table)
+df = df[df['status.reason.longKey'] == 'finished']
 
-all_scripts = soup.find_all('script')
-
-matches_script = all_scripts[32].text
-
-data = json.loads(matches_script)
-
-maclar = data['props']['pageProps']['matches']['allMatches']
+i = 0
 
 df3 = []
 
-for i in range(len(maclar)):
+while i < len(df):
 
-    df = pd.DataFrame(data['props']['pageProps']['matches']['allMatches'][i])
+    match_id = df['id'].iloc[i]
 
-    if (df['status']['finished'] == True):
+    with urllib.request.urlopen("https://www.fotmob.com/api/matchDetails?matchId=" + str(match_id)) as url:
+        data_url = json.load(url)
 
-        match_id = df['id'][0]
-
-        with urllib.request.urlopen("https://www.fotmob.com/api/matchDetails?matchId=" + str(match_id)) as url:
-            data_url = json.load(url)
-
-        df2 = pd.DataFrame(data_url["content"]["shotmap"]["shots"])
-        lig_df = pd.DataFrame(data_url["general"])
-        league_name = str(lig_df["parentLeagueName"].iloc[0])
-        league_season = str(lig_df["parentLeagueSeason"].iloc[0])
-        df2["league_name"] = league_name
-        df2["league_season"] = league_season
-        df2["match_id"] = match_id
-        df3.append(df2)
+    df2 = pd.DataFrame(data_url["content"]["shotmap"]["shots"])
+    lig_df = pd.DataFrame(data_url["general"])
+    league_name = str(lig_df["parentLeagueName"].iloc[0])
+    league_season = str(lig_df["parentLeagueSeason"].iloc[0])
+    df2["league_name"] = league_name
+    df2["league_season"] = league_season
+    df2["match_id"] = match_id
+    df3.append(df2)
 
 final_df = pd.concat(df3)
 
